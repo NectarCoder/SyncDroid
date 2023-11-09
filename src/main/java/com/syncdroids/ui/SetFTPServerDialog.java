cb58972f576484105db4df5e1e8c2659b6815345
NectarCoder <109771600+NectarCoder@users.noreply.github.com>
2023-11-08 22:24:35 -0500

    Stub files for the set ftp server dialog

---
+++ src/main/java/com/syncdroids/ui/SetFTPServerDialog.java
@@ -0,0 +1,13 @@
+package com.syncdroids.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class SetFTPServerDialog extends Dialog<Stage> {
+    public MenuItem exit;
+
+
+
+}
