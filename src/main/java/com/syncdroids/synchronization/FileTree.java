addfed4431c3e88b4b0e5c99bcb6a3ceeafa7060
NectarCoder <109771600+NectarCoder@users.noreply.github.com>
2023-11-23 14:35:50 -0500

    Completed FileTree and FileNode classes
    - added equals() implementation for both
    - added a test directory for testing

--- src/main/java/com/syncdroids/synchronization/FileTree.java
+++ src/main/java/com/syncdroids/synchronization/FileTree.java
@@ -1,35 +1,26 @@
 package com.syncdroids.synchronization;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 
-public class FileTree implements Comparable<FileTree> {
+public class FileTree {
 
     private FileNode root;
+
     public FileTree(String path) throws FileNotFoundException {
         this.root = new FileNode(path);
     }
 
-    @Override
-    public int compareTo(FileTree o) {
-        return 0;
+    public FileNode getFileRoot() {
+        return this.root;
     }
 
-
-
-    /*
-    public boolean parsePath(boolean forceParse) {
-        if (subFilesAndFolders != null) {
-            if (forceParse) {
-                //re parse subFiles...
-            } else {
-                // don't do anything
-            }
-
+    public boolean equals(Object o) {
+        // Input must be of type FileNode
+        if (o.getClass() != FileTree.class) {
+            return false;
         } else {
+            FileTree other = (FileTree) o;
+            return this.root.equals(other.getFileRoot());
         }
     }
-     */
-
-
 }
