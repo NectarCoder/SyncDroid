1936cef8247c04eb94d08f1a63d255fee0740a83
NectarCoder <109771600+NectarCoder@users.noreply.github.com>
2023-11-28 21:19:50 -0500

    Minor updates
    - to module info
    - removed unneeded actions in File menu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,7 @@ module com.syncdroids {
     requires org.apache.commons.io;
 
     exports com.syncdroids.ui;
+    exports com.syncdroids.fileengine;
+    exports com.syncdroids.fileengine.exception;
     opens com.syncdroids.ui to javafx.fxml;
 }
